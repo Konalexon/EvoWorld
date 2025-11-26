fcadd81c3b5e3ae347ba65e61f94f8dcd9efdd35
Konrad Lasota <konalinio@gmail.com>
2025-11-26 23:35:31 +0100

    feat: Set up initial project structure by adding basic HTML, CSS, and JS, and removing Phaser imports from main.js.

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-import Phaser from 'phaser';
-import LobbyScene from './LobbyScene.js';
-import GameScene from './game.js';
-
-const config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
+height: window.innerHeight,
     backgroundColor: '#2d2d2d',
-    parent: 'app',
-    dom: {
-        createContainer: true
-    },
-    physics: {
+        parent: 'app',
+            dom: {
+    createContainer: true
+},
+physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: [LobbyScene, GameScene]
+        gravity: { y: 0 },
+        debug: false
+    }
+},
+scene: [LobbyScene, GameScene]
 };
 
 const game = new Phaser.Game(config);
